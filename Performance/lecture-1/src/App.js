1e54c49d79a5369808e08e7cde99f7aead142dca
SeungHyun Kim <daejjyu@gmail.com>
2022-01-03 04:19:47 +0900

    react lazy 적용으로 번들 분리

--- Performance/lecture-1/src/App.js
+++ Performance/lecture-1/src/App.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
+import React, { Suspense, lazy } from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+// import ListPage from './pages/ListPage/index'
+// import ViewPage from './pages/ViewPage/index'
+
+const ListPage = lazy(() => import("./pages/ListPage/index"));
+const ViewPage = lazy(() => import("./pages/ViewPage/index"));
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" component={ListPage} exact />
-        <Route path="/view/:id" component={ViewPage} exact />
+        <Suspense fallback={<div>로딩중</div>}>
+          <Route path="/" component={ListPage} exact />
+          <Route path="/view/:id" component={ViewPage} exact />
+        </Suspense>
       </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
